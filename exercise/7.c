67afc92c339b6e12a99c3da78178f2d0f2d973f8
w2807 <w2807090881@outlook.com>
2022-11-03 20:39:13 +0800

    git commit

--- exercise/7.c
+++ exercise/7.c
@@ -2,10 +2,11 @@
 #include<string.h>
 int main()
 {
-    char str[20]={0};
+    char str[20]={0},str1[20]={0};
     int i;
     printf("input a string:");
     gets(str);
+    strcpy(str1,str);
     for(i=0;i<strlen(str);i++)
     if(a[i]>=48&&a[i]<=57)
     {
