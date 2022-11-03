d0b8949e0ff47980af0f3775c829c5419f265433
w2807 <w2807090881@outlook.com>
2022-11-03 20:39:32 +0800

    git commit

--- exercise/7.c
+++ exercise/7.c
@@ -3,13 +3,19 @@
 int main()
 {
     char str[20]={0},str1[20]={0};
-    int i;
+    int i,j;
     printf("input a string:");
     gets(str);
     strcpy(str1,str);
-    for(i=0;i<strlen(str);i++)
-    if(a[i]>=48&&a[i]<=57)
+    for(i=0,j=0;i<strlen(str);i++)
+    if(str1[i]>='0'&&str1[i]<='9')
     {
-         
+        str[j++]='@';
+        str[j++]=str1[i];
     }
+    else
+    str[j++]=str1[i];
+    str[j]='\0';
+    puts(str);
+    return 0;
 }
