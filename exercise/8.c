8226bfdf1e94fd4ffc1050ef39990222624bb6a4
w2807 <w2807090881@outlook.com>
2022-11-04 17:19:09 +0800

    git commit

--- exercise/8.c
+++ exercise/8.c
@@ -2,15 +2,36 @@
 #include<string.h>
 int main()
 {
-    int n,i,j,k;
+    int n,i,j,k,l,m,max,min;
     printf("input n:");
-    scnaf("%d",&n);
+    scanf("%d",&n);
     int a[n][n];
+    printf("input array:\n");
     for(i=0;i<n;i++)
     for(j=0;j<n;j++)
     scanf("%d",&a[i][j]);
     for(i=0;i<n;i++)
     {
-        
+        max=a[i][0];
+        for(j=0;j<n;j++)
+        if(a[i][j]>max)
+        {
+            max=a[i][j];
+            k=j;
+        }
+        min=a[i][k];
+        for(l=0;l<n;l++)
+        if(a[l][k]<min)
+        {
+            min=a[l][k];
+            m=l;
+        }
+        if(max==min)
+        break;
     }
+    if(max==min)
+    printf("a[%d][%d]=%d",m,k,max);
+    else
+    printf("NO");
+    return 0;
 }
