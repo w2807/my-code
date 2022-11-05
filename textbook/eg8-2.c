dcaf8839eff21607031e5c02a2ce30defc83eb4d
w2807 <w2807090881@outlook.com>
2022-11-05 14:55:41 +0800

    git commit

---
+++ textbook/eg8-2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+int main()
+{
+    int a;
+    char *s;
+    printf("input a string and reverse it:");
+    s=(char*)malloc(80);
+    if(!s)
+    {
+        printf("memory requst failed");
+        exit(1);
+    }
+    gets(s);
+    for(a=strlen(s)-1;a>=0;a--)
+    putchar(s[a]);
+    free(s);
+    return 0;
+}
