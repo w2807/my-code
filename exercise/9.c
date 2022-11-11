f918945c28b97e592c3ef02cad9fba6bf3aeaf78
w2807 <w2807090881@outlook.com>
2022-11-11 12:35:06 +0800

    git commit

--- exercise/9.c
+++ exercise/9.c
@@ -1,7 +1,29 @@
 #include<stdio.h>
-int erfen(int a[],int b);
+int erfen(int a[],int b,int start,int end);
 int main()
 {
-    int a[10]={1,2,3,4,5,6,7,8,9,10},i;
-    
+    int a[10]={1,2,3,4,5,6,7,8,9,10},i,j;
+    printf("input a number:");
+    scanf("%d",&i);
+    j=erfen(a,i,0,9);
+    if(j==-1)
+    printf("not found\n");
+    else
+    printf("a[%d]=%d",j,i);
+    return 0;
+}
+int erfen(int a[],int b,int start,int end)
+{
+    int *p=a,mid;
+    while(start<=end)
+    {
+        mid=(start+end)/2;
+        if(b==*(p+mid))
+        return mid;
+        else if(b>*(p+mid))
+        start=mid+1;
+        else if(b<*(p+mid))
+        end=mid-1;
+    }
+    return -1;
 }
